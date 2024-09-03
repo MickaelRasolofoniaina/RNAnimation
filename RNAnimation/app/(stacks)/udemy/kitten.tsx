7bc9cf09b235f20b28fa6e67aec68a759bd494fa
RASOLOFONIAINA Mickael <rasolofoniaina_mickael@outlook.fr>
2024-09-03 22:08:24 +0300

    Remove global var

--- RNAnimation/app/(stacks)/udemy/kitten.tsx
+++ RNAnimation/app/(stacks)/udemy/kitten.tsx
@@ -42,6 +42,37 @@ const KITTENS: Kitten[] = [
 ];
 
 export default function Kitten() {
+	const KITTENS: Kitten[] = [
+		{
+			id: 1,
+			image:
+				"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Juvenile_Ragdoll.jpg/1200px-Juvenile_Ragdoll.jpg",
+			description: "White kitten",
+			scale: new Animated.Value(0.8),
+		},
+		{
+			id: 2,
+			image:
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF08wbZeJDlwbOTkKlnNYcdKve_Y2lCasOEQ&s",
+			description: "Yellow kitten",
+			scale: new Animated.Value(0.8),
+		},
+		{
+			id: 3,
+			image:
+				"https://assets-global.website-files.com/62604bb9b6d9d12008ba1216/627d0cbe0c5bbd447a635839_new-kitten-checklist.jpg",
+			description: "Three black yellow kitten",
+			scale: new Animated.Value(0.8),
+		},
+		{
+			id: 4,
+			image:
+				"https://www.whiskas.co.uk/cdn-cgi/image/format=auto,q=90/sites/g/files/fnmzdf5651/files/2022-09/Kitten-mobile.jpg",
+			description: "Black grey kitten",
+			scale: new Animated.Value(1),
+		},
+	];
+
 	const [kittens, setKittens] = useState(KITTENS);
 
 	const onRemove = () => {
